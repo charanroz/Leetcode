fae09e4a9aacd8bbaf7522bba2a51adf1cd5f02b
charanroz <rozcharan@gmail.com>
2026-06-14 20:42:52 +0100

    121 Solution

---
+++ src/BuySell.java
@@ -0,0 +1,24 @@
+public class BuySell {
+
+    //    121. Best Time to Buy and Sell Stock
+    //    https://leetcode.com/problems/best-time-to-buy-and-sell-stock/?envType=problem-list-v2&envId=dynamic-programming
+
+    static void main(String[] args) {
+        int[] prices = {7,1,5,3,6,4};
+        System.out.println(maxProfit(prices));
+    }
+    public static int maxProfit(int[] prices) {
+        int buy = prices[0];
+        int profit = 0;
+
+        for (int i = 1; i <prices.length ; i++) {
+            if (prices[i] < buy){
+                buy = prices[i];
+            }
+            else if (prices[i] - buy > profit){
+                profit = prices[i]-buy;
+            }
+        }
+        return profit;
+    }
+}
